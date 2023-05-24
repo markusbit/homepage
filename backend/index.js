a82272f9f754db61ec0152d7b14f5634ccc77597
Markus Brandstetter <markus.brandstetter@it.htlhl.at>
2023-05-24 10:06:28 +0200

    docker

--- backend/index.js
+++ backend/index.js
@@ -13,13 +13,14 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors()); 
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+// API Routes
 app.use('/api/auth', authRouter); 
 app.use('/api/contact', contactRouter); 
 
-app.use(express.static(path.join(__dirname, '../../fronted/build'))); 
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build/index.html'));
 });
 
 const port = process.env.PORT;
