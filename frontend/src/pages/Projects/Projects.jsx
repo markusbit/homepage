4d7b439007b04838bf544233e4a212c4cbea8e01
Markus Brandstetter <markus.brandstetter@it.htlhl.at>
2023-06-13 14:07:44 +0200

    projects added homepage

--- frontend/src/pages/Projects/Projects.jsx
+++ frontend/src/pages/Projects/Projects.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import ProjectCard from "../../components/Card"; 
+import ProjectCard from "../../components/Card";
 
 function Projects() {
     return (
         <div className="page-content">
-            <ProjectCard className="card" title="Farbenhaus Retz-Hollabrunn" description="Farbenhaus Retz-Hollabrunn" demoLink="http://farbenhaus-retz.at/" imgPath="http://farbenhaus-retz.at/img/farbenhaus-logo.jpg" />
+            <div className="d-flex justify-content-between">
+                <ProjectCard className="card" title="Farbenhaus Retz-Hollabrunn" description="Farbenhaus Retz-Hollabrunn" demoLink="http://farbenhaus-retz.at/" imgPath="http://farbenhaus-retz.at/img/farbenhaus-logo.jpg" />
+                <ProjectCard className="card" title="My Homepage" description="Check out the code to my homepage" demoLink="https://github.com/markusbit/homepage/" imgPath="" />
+            </div>
         </div>
     )
 }
