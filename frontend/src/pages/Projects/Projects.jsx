46dab9fa5c3be96caf762e345a462e28e6e7cd87
Markus Brandstetter <markus.brandstetter@it.htlhl.at>
2023-06-14 07:56:44 +0200

    projects

--- frontend/src/pages/Projects/Projects.jsx
+++ frontend/src/pages/Projects/Projects.jsx
@@ -6,7 +6,7 @@ function Projects() {
         <div className="page-content">
             <div className="d-flex justify-content-between">
                 <ProjectCard className="card" title="Farbenhaus Retz-Hollabrunn" description="Farbenhaus Retz-Hollabrunn" demoLink="http://farbenhaus-retz.at/" imgPath="http://farbenhaus-retz.at/img/farbenhaus-logo.jpg" />
-                <ProjectCard className="card" title="My Homepage" description="Check out the code to my homepage" demoLink="https://github.com/markusbit/homepage/" />
+                <ProjectCard className="card" title="My Homepage" description="Check out my homepage on Github" demoLink="https://github.com/markusbit/homepage/" />
             </div>
         </div>
     )
